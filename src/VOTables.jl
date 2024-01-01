7ca1889e0ec90be5ecb2bd820ad00df0cc5da58d
Alexander Plavin <alexander@plav.in>
2024-01-01 00:00:00 +0000

    actual implementation

--- src/VOTables.jl
+++ src/VOTables.jl
@@ -1,3 +1,100 @@
 module VOTables
 
+using EzXML
+using DictArrays
+using Dictionaries
+using DataPipes
+using Accessors #Extra  # Extra - for Dictionaries support
+
+
+read(votfile) = let
+	tblx = tblxml(votfile)
+	_fieldattrs = fieldattrs(tblx)
+	res = @p _fieldattrs |> map(Symbol(_["name"]) => Union{vo2jltype(_),Missing}[]) |> dictionary |> DictArray
+	_filltable!(res, tblx)
+	@modify(col -> map(identity, col), res |> Properties())
+end
+
+_filltable!(res, tblx) = @p let
+	tblx
+	@aside ns = ["ns" => namespace(__)]
+	findall("ns:DATA/ns:TABLEDATA", __, ns)
+	only
+	findall("ns:TR", __, ns)
+	foreach() do tr
+		map(AbstractDictionary(res), eachelement(tr)) do col, td
+			@assert nodename(td) == "TD"
+			val = _parse(eltype(col), nodecontent(td))
+			push!(col, val)
+		end
+	end
+end
+
+
+tblxml(votfile) = @p let
+	Base.read(votfile, String)
+	parsexml
+	root
+	@aside ns = ["ns" => namespace(__)]
+	findall("ns:RESOURCE/ns:TABLE", __, ns)
+	only
+end
+
+description(tblxml) = @p let
+	tblxml
+	@aside ns = ["ns" => namespace(__)]
+	findall("ns:DESCRIPTION", __, ns)
+	only
+	nodecontent
+end
+
+fieldattrs(tblxml) = @p let
+	tblxml
+	@aside ns = ["ns" => namespace(__)]
+	findall("ns:FIELD", __, ns)
+	map() do fieldxml
+		attrs = @p attributes(fieldxml) |> map(nodename(_) => nodecontent(_)) |> dictionary
+		desc = @p fieldxml |> findall("ns:DESCRIPTION", __, ns) |> only |> nodecontent
+		insert!(attrs, "description", desc)
+		return attrs
+	end
+end
+
+TYPE_VO_TO_JL = Dict(
+    "boolean" => Bool,
+    "bit" => Bool,
+    "unsignedByte" => UInt8,
+    "char" => Char,
+    "unicodeChar" => Char,
+    "short" => Int16,
+    "int" => Int32,
+    "long" => Int64,
+    "float" => Float32,
+    "double" => Float64,
+    "floatComplex" => ComplexF32,
+    "doubleComplex" => ComplexF64,
+)
+
+function vo2jltype(attrs)
+	if get(attrs, "arraysize", "1") == "1"
+		TYPE_VO_TO_JL[attrs["datatype"]]
+	elseif attrs["datatype"] == "char"
+		@assert occursin(r"^\d+$", attrs["arraysize"])
+		String
+	else
+		@assert occursin(r"^\d+$", attrs["arraysize"])
+		Vector{TYPE_VO_TO_JL[attrs["datatype"]]}
+	end
+end
+
+
+_parse(::Type{Union{Missing, T}}, s) where {T} = _parse(T, s)
+_parse(::Type{Union{Missing, T}}, s::Missing) where {T} = missing
+_parse(::Type{Union{Missing, Char}}, s) = isempty(s) ? missing : only(s)
+_parse(::Type{Union{Missing, Char}}, ::Missing) = missing  # disambiguation
+
+_parse(::Type{T}, s) where {T} = parse(T, s)
+_parse(::Type{Char}, s) = only(s)
+_parse(::Type{String}, s) = s
+
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,10 +2,21 @@ using TestItems
 using TestItemRunner
 @run_package_tests
 
+@testitem "read" begin
+    votgzfile = joinpath(@__DIR__, "data/votable?-source=J%2FApJ%2F923%2F67%2Ftable2&-out=**&-out.max=100.gz")
+    votfile = tempname()
+    run(pipeline(`gunzip -ck $votgzfile`, stdout=votfile))
+    tbl = VOTables.read(votfile)
+    @test length(tbl) == 100
+    @test tbl.recno == 1:100
+    @test eltype(tbl.Tb) == Float32
+    @test eltype(tbl.l_Tb) == Union{Missing,Char}
+    @test tbl[5].Bpa === -9.1f0
+end
 
 @testitem "_" begin
     import Aqua
-    Aqua.test_all(VOTables; ambiguities=false)
+    Aqua.test_all(VOTables; ambiguities=false, unbound_args=false)
     Aqua.test_ambiguities(VOTables)
 
     import CompatHelperLocal as CHL
