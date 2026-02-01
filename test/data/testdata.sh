7208aa2d05d7104e476db67c74cfd686d17ce6ee
Alexander Plavin <alexander@plav.in>
2026-02-01 00:00:00 +0000

    add test for reading complex

--- test/data/testdata.sh
+++ test/data/testdata.sh
@@ -18,3 +18,20 @@ do
                               cmd='delcols "f_string v_string"' \
                               ofmt="votable(format=$Fmt)" out=$file
 done
+
+# Complex type test files.
+# STILTS :test: generator doesn't support complex types, so we convert
+# from a hand-written TABLEDATA source file. Note: STILTS outputs complex
+# columns as arraysize="2" float/double in binary — the field metadata
+# must be manually fixed to floatComplex/doubleComplex (binary data is identical).
+for fmt in binary binary2
+do
+   file=complex-${fmt}.vot
+   echo "Writing $file"
+   Fmt=`echo $fmt | tr a-z A-Z`
+   java -jar stilts.jar tpipe in=complex-tabledata.vot \
+                              ofmt="votable(format=$Fmt)" out=$file
+   # Fix: STILTS writes complex as arraysize="2" float/double, restore floatComplex/doubleComplex
+   sed -i '' 's/arraysize="2" datatype="float" name="s_floatComplex"/datatype="floatComplex" name="s_floatComplex"/' $file
+   sed -i '' 's/arraysize="2" datatype="double" name="s_doubleComplex"/datatype="doubleComplex" name="s_doubleComplex"/' $file
+done
