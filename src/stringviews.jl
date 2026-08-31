83c25fba69ad33114736da698f62e4e6295e1c62
Kristoffer Carlsson <kcarlsson89@gmail.com>
2026-08-31 14:32:39 +0200

    Fix StringView ambiguity on Julia 1.14

--- src/stringviews.jl
+++ src/stringviews.jl
@@ -1,7 +1,7 @@
 # XXX: to upstream to StringViews
 
 # https://github.com/JuliaStrings/StringViews.jl/pull/23
-function Base.unsafe_wrap(::Type{<:StringViews.StringView}, cstr::Cstring)
+function Base.unsafe_wrap(::Type{<:StringView}, cstr::Cstring)
     ptr = convert(Ptr{UInt8}, cstr)
     i = findfirst(i -> unsafe_load(ptr, i) == 0x00, 1:typemax(Int)) - 1
     return StringView(UnsafeArray(ptr, (i,)))
